177881798f86d091b99ad324e16bf1cc5d7eabfb
Vlad Susidko <93520329+exemory@users.noreply.github.com>
2023-09-30 15:23:49 +0300

    Setup Identity with MongoDB store

---
+++ backend/src/Application/Options/MongoDBOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Options;
+
+public class MongoDBOptions
+{
+    public const string SectionName = "MongoDBOptions";
+    
+    public required string ConnectionString { get; set; }
+    public required string DatabaseName { get; set; }
+}
