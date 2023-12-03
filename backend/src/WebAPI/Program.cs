c0e4974868e9b550e3153a613086e8374b538c71
Vlad Susidko <93520329+exemory@users.noreply.github.com>
2023-12-03 15:34:27 +0200

    Add health check endpoint

--- backend/src/WebAPI/Program.cs
+++ backend/src/WebAPI/Program.cs
@@ -1,11 +1,15 @@
 using Application;
-using WebAPI.Infrastructure;
+using WebAPI;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddWebApiServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.MapHealthChecks();
 
 app.UseAuthentication();
 app.UseAuthorization();
