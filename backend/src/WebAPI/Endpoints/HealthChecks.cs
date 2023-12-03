079055ddcd0a7ecdf87fca53c003992cb251838e
Vlad Susidko <93520329+exemory@users.noreply.github.com>
2023-12-03 17:33:04 +0200

    Fix bug with GraphQL endpoint double registration, move health checks endpoint mapping to a separate endpoint group

---
+++ backend/src/WebAPI/Endpoints/HealthChecks.cs
@@ -0,0 +1,16 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using WebAPI.Infrastructure;
+
+namespace WebAPI.Endpoints;
+
+public class HealthChecks : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapHealthChecks("/healthz", new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    }
+}
