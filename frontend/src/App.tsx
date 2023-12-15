48152f584eda701d514130e16dc1f20b2247560d
Nikita Panagoda <nkt.pngd@gmail.com>
2023-12-15 21:19:41 +0200

    Add alias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="font-bold text-2xl text-center">Hello world!</div>;
+  return <div className="font-bold text-2xl text-center text-red-500">Hello world!</div>;
 }
 
 export default App;
