b313ccd42b2c4c08c508f25b7c8dbe824a707c8b
Vlad Susidko <93520329+exemory@users.noreply.github.com>
2024-01-08 09:11:10 +0200

    Update question group ids

--- backend/src/Application/Common/Constants/GeneralTestIdentifiers.cs
+++ backend/src/Application/Common/Constants/GeneralTestIdentifiers.cs
@@ -4,10 +4,10 @@ public static class GeneralTestIdentifiers
 {
     public const string TestId = "657d7272a24def72b8419b84";
 
-    public static readonly Guid GeneralQuestionGroupId = Guid.Parse("4ec6f0cb-4b8f-484b-8c5f-5fac2e83132b");
-    public static readonly Guid MoralTraumaQuestionGroupId = Guid.Parse("1f5dd186-b8ce-493c-930e-8b173a053182");
-    public static readonly Guid PtsdQuestionGroupId = new();
-    public static readonly Guid PcsQuestionGroupId = new();
-    public static readonly Guid CtsdQuestionGroupId = new();
-    public static readonly Guid SsQuestionGroupId = new();
+    public static readonly Guid GeneralQuestionGroupId = Guid.Parse("6d301487-4f10-4f3f-bd15-b152ced130f6");
+    public static readonly Guid MoralTraumaQuestionGroupId = Guid.Parse("449ccb47-8cfd-45f7-b70e-b3f121246ad5");
+    public static readonly Guid PtsdQuestionGroupId = Guid.Parse("95fbefde-60b3-4f5a-a80d-36f2b515fe65");
+    public static readonly Guid PcsQuestionGroupId = Guid.Parse("53efb497-dc53-4c40-8dd7-8f9f5c8bbe64");
+    public static readonly Guid CtsdQuestionGroupId = Guid.Parse("4d848119-5de3-4fd1-84f3-2989f6975f16");
+    public static readonly Guid SsQuestionGroupId = Guid.Parse("f1a6e581-7503-49b7-a4ac-2bbc156a5765");
 }
