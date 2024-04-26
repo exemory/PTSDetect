fc02decc9bad4de308ff109325f2e6b1f37df742
Nikita Panagoda <nkt.pngd@gmail.com>
2024-04-26 15:08:50 +0300

    Update general test ui/ux

--- frontend/src/pages/GeneralTest/GeneralTest.tsx
+++ frontend/src/pages/GeneralTest/GeneralTest.tsx
@@ -1,13 +1,17 @@
 import { Question } from '@/__generated__/graphql';
 import { GET_GENERAL_TEST_QUESTIONS } from '@/graphql/queries/getGeneralTestQuestions';
 import { useLazyQuery } from '@apollo/client';
-import { Button, CircularProgress, FormControl, FormLabel, Radio, RadioGroup, Typography } from '@mui/joy';
+import { Button, CircularProgress, FormControl, List, ListItem, Radio, RadioGroup, Typography } from '@mui/joy';
+import { ArrowRight } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export const GeneralTest = () => {
   const [questions, setQuestions] = useState<Question[] | undefined>([]);
   const [endCursor, setEndCursor] = useState<string | undefined>(undefined);
+  const [hasNextPage, setHasNextPage] = useState<boolean | undefined>(undefined);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [totalQuestions, setTotalQuestions] = useState(0);
 
   const { register, handleSubmit } = useForm({});
 
@@ -19,7 +23,7 @@ export const GeneralTest = () => {
         input: {
           languageCode: 'en',
         },
-        first: 5,
+        first: 1,
       },
     });
   }, []);
@@ -28,19 +32,26 @@ export const GeneralTest = () => {
     if (data) {
       const newQuestions = data.generalTestQuestions.questions?.nodes || [];
       const currentQuestions = questions || [];
+
       setQuestions([...currentQuestions, ...newQuestions]);
       setEndCursor(data.generalTestQuestions.questions?.pageInfo.endCursor ?? '');
+      setHasNextPage(data.generalTestQuestions.questions?.pageInfo.hasNextPage ?? false);
+      setTotalQuestions(data.generalTestQuestions.questions?.totalCount || 0);
+
+      if (questions && questions?.length >= 1) {
+        setCurrentQuestion((currState) => currState + 1);
+      }
     }
   }, [data]);
 
-  const loadMore = () => {
+  const loadNextQuestion = () => {
     getQuestions({
       variables: {
         input: {
           languageCode: 'en',
         },
         after: endCursor,
-        first: 5,
+        first: 1,
       },
     });
   };
@@ -51,44 +62,78 @@ export const GeneralTest = () => {
 
   if (loading && !questions?.length) {
     return (
-      <div>
+      <div className="flex flex-col items-center mx-auto gap-2 max-w-[660px]">
         <CircularProgress />
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col mx-auto gap-8 max-w-[960px]">
-      <div className="flex items-center justify-between">
-        <Typography level="h2">Comprehensive Post-Traumatic Stress Disorder Evaluation</Typography>
-        <Typography level="h2">3/10</Typography>
-      </div>
+    <div className="flex flex-col mx-auto gap-2 max-w-[660px]">
+      <Typography level="title-md" color="primary">
+        Question {currentQuestion + 1} of {totalQuestions}
+      </Typography>
+
       <form onSubmit={onSubmit}>
         <div className="flex flex-col gap-4">
-          {questions &&
-            questions.map((question) => (
-              <FormControl key={question.id}>
-                <div className="flex flex-col gap-2 border border-1 p-6 rounded-lg">
-                  <FormLabel>
-                    <Typography fontSize={21} level="title-lg">
-                      {question.title}
-                    </Typography>
-                  </FormLabel>
-                  <RadioGroup {...register(question.id)}>
-                    <div className="flex flex-col gap-4">
-                      {question.answers?.map((answer) => (
-                        <Radio key={answer.id} value={answer.id} label={answer.title} {...register(question.id)} />
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-              </FormControl>
-            ))}
+          {questions?.length && (
+            <FormControl key={questions[currentQuestion].id}>
+              <Typography level="h2" marginBottom={2}>
+                {questions[currentQuestion].title}
+              </Typography>
 
-          <div className="flex flex-col mt-6">
-            <Button loading={loading} onClick={loadMore} fullWidth>
-              Load More Questions
-            </Button>
+              <RadioGroup
+                {...register(questions[currentQuestion].id)}
+                size="lg"
+                sx={{
+                  gap: 2,
+                  width: '100%',
+                }}
+              >
+                <List
+                  sx={{
+                    '--List-gap': '0.5rem',
+                    '--ListItem-paddingY': '1rem',
+                    '--ListItem-radius': '8px',
+                    '--ListItemDecorator-size': '32px',
+                  }}
+                >
+                  {questions[currentQuestion].answers?.map((answer) => (
+                    <ListItem variant="outlined" key={answer.id} sx={{ boxShadow: 'sm' }}>
+                      <Radio
+                        {...register(questions[currentQuestion].id)}
+                        key={answer.id}
+                        value={answer.id}
+                        label={answer.title}
+                        overlay
+                        sx={{ flexGrow: 1, flexDirection: 'row-reverse' }}
+                        slotProps={{
+                          action: ({ checked }) => ({
+                            sx: (theme) => ({
+                              ...(checked && {
+                                inset: -1,
+                                border: '2px solid',
+                                borderColor: theme.vars.palette.primary[500],
+                              }),
+                            }),
+                          }),
+                        }}
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </RadioGroup>
+            </FormControl>
+          )}
+
+          <div className="flex flex-col items-end">
+            {hasNextPage ? (
+              <Button loading={loading} onClick={loadNextQuestion} size="lg" fullWidth endDecorator={<ArrowRight />}>
+                Next
+              </Button>
+            ) : (
+              <Button type="submit">Finish</Button>
+            )}
           </div>
         </div>
       </form>
