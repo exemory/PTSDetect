36b379c4570907de0c20e1cff2799e445a4454a4
Vlad Susidko <93520329+exemory@users.noreply.github.com>
2024-10-05 19:29:16 +0300

    Add query to retrieve all users, add Administrator role

---
+++ backend/src/Application/Features/User/Users.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Constants;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using HotChocolate;
+using HotChocolate.Authorization;
+using HotChocolate.Types;
+
+namespace Application.Features.User;
+
+[ExtendObjectType(GraphQlTypes.Query)]
+public class UsersQuery
+{
+    [Authorize(Roles = [Roles.Administrator])]
+    public async Task<IList<UserInfo>> Users(
+        [Service] IUserService userService,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await userService.GetUsersAsync(cancellationToken);
+        return result.Value;
+    }
+}
